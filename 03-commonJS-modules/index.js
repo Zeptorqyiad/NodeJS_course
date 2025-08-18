096ebca3bfde65a0c40d6564541339d994dc4a3e
Zeptorqyiad <Zeptorqyiad99@gmail.com>
2025-08-18 22:21:05 +0500

    update new third lessons

---
+++ 03-commonJS-modules/index.js
@@ -0,0 +1,8 @@
+console.log(arguments.callee.toString())
+console.log(module)
+console.log(__filename)
+console.log(__dirname)
+console.log(exports)
+console.log(module.exports)
+console.log(module.exports === exports)
+console.log(require)
