c7683b7bb9225de35a89f65b6d91cd802939ee55
Zeptorqyiad <Zeptorqyiad99@gmail.com>
2025-09-01 23:16:36 +0500

    new update

--- 03-commonJS-modules/index.js
+++ 03-commonJS-modules/index.js
@@ -1,8 +1,15 @@
 const { myName, myHobbies, myFavoriteNumber } = require("./multiple-exports")
+const { myName: myOtherName, myFriendsName, myGreatHobbies } = require("./export-and-import")
 const greetingFn = require("./single-export")
 
 console.log(myName)
 console.log(myHobbies)
 console.log(myFavoriteNumber)
 
+myHobbies.push("climbing")
+
 greetingFn(myName)
+
+console.log(myOtherName)
+console.log(myFriendsName)
+console.log(myGreatHobbies)
